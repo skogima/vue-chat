3d8c41f6bfbf700584331f705e13271ff8e67fc6
Gabriel Kogima <gabriel_kogima@hotmail.com>
2021-07-01 16:54:32 -0300

    change env variables and add env example file

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,7 +1,7 @@
 import SockJS from "sockjs-client";
 import Stomp from "webstomp-client";
 
-const sock = new SockJS(process.env.VUE_APP_CHAT_URL);
+const sock = new SockJS(`${process.env.VUE_APP_API_URL}/join`);
 const stomp = Stomp.over(sock, {
     debug: false,
 });
